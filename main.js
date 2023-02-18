60dac15497329a092bdeeb4aed3a9ad9aed67ce5
Anurag Gaur <anuraggaur495@gmail.com>
2023-02-18 11:23:11 +0530

    Day-4 changes

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
+// navigate to top button functionality
+const navToTopBtn = document.querySelector('.home-navigation-button');
+navToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    navToTopBtn.style.display = 'block';
+  } else {
+    navToTopBtn.style.display = 'none';
+  }
+}
+
+// modal window for book appointment
+const modal = document.querySelector('.modal');
+const modalWindow = document.querySelector('.modal-window');
+const bookAppointmentBtn = document.querySelector('.book-appointment');
+const getStartedtBtn = document.querySelector('.get-started');
+
+const modalContent = `<h1>Great Decision!</h1><p>You're one click away from experiencing the best grooming service in town!</p><a href="tel:+911234567898">Book it!</a>`;
+
+const toggleModal = () => {
+    modal.style.display = 'flex';
+    modal.style.justifyContent = 'center';
+    modal.style.alignItems = 'center';
+    modalWindow.insertAdjacentHTML('afterbegin', modalContent);
+    modalWindow.style.color = 'tomato';
+}
+
+bookAppointmentBtn.addEventListener('click', toggleModal, false);
+
+// temporary fix. Do remove
+// modal.addEventListener('click', () => {
+//     modal.style.display = 'none';
+//     modal.style.removeProperty('justify-content');
+//     modal.style.removeProperty('align-items');
+//     modalWindow.innerHTML = '';
+// });
---
+++ readme.md
@@ -0,0 +1,29 @@
+Make nav items clickable -----
+
+Add a home navigation button ------
+
+Add functionality to book appointments and get started buttons 
+
+Change body and header fonts and colors (use variables)
+
+Resize hero image
+
+Link video
+
+Lazy loading on images
+
+Optimize all images for the web
+
+Service price list modal window
+
+Our team modal window
+
+enlarge gallery images on hover ------
+
+adjust styling for smaller screen
+
+adjust styling for mobile devices
+
+services and meet the team carousels (for mobile)
+
+Display all the appropriate attributions
